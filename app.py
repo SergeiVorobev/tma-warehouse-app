10ffc9cc15612b0225fee5a91baa66c2f2d75092
Sergey Vorobev <vorobevse86@gmail.com>
2024-04-18 00:16:05 +0200

    feat: added initial tests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, abort, session
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 import sqlite3
 
 from config import Config
